dc5aec24b89e38bbac13f01e86a897f823c6aa47
Jordan <jordan.milne2@gmail.com>
2020-05-06 14:55:51 -0400

    first package upload

---
+++ tests/test_zucked.py
@@ -0,0 +1,35 @@
+import zucked
+
+def test_words():
+    d = [{
+      "participants": [
+        {
+          "name": "User2"
+        },
+        {
+          "name": "User1"
+        }
+      ],
+      "messages": [
+        {
+          "sender_name": "User2",
+          "timestamp_ms": 1502384278819,
+          "content": "hey this is a silly test",
+          "type": "Generic"
+        },
+        {
+          "sender_name": "User1",
+          "timestamp_ms": 1502383930331,
+          "content": "yeah it is, yeah haha",
+          "type": "Generic"
+        },
+      ],
+      "title": "User2",
+      "is_still_participant": True,
+      "thread_type": "Regular",
+      "thread_path": "inbox/User2"
+    }]
+    top_words = [('yeah', 2), ('it', 1), ('is', 1), ('haha', 1)]
+    assert zucked.top_50_words(d,'User1') == top_words
+    
+
