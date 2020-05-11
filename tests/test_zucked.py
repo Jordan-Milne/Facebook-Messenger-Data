1c56952b4b4babae15b80ea535316ee1e9ac3228
Jordan <jordan.milne2@gmail.com>
2020-05-11 10:59:46 -0400

    updated version

--- tests/test_zucked.py
+++ tests/test_zucked.py
@@ -1,6 +1,21 @@
 import zucked as zd
 
-def test_words():
+def test_top_words():
     top_words = [('hi', 3), ('lol', 2), ('one', 1)]
     ms = zd.read_messages
-    assert ms.top_50_words('Jordan Milne') == top_words
+    assert ms.top_words('Jordan Milne') == top_words
+
+
+def test_top_convos():
+    top_convo = [('Zark Muckerberg', 1)]
+    ms = zd.read_messages
+    assert ms.top_convos('Jordan Milne') == top_convo
+
+
+def test_search():
+    search = [{'Message': 'hi hi hi lol lol one',
+                  'Sent to': 'Zark Muckerberg',
+                  'Date': '2016-06-09 11:46:54'}]
+
+    ms = zd.read_messages
+    assert ms.search_messages('Jordan Milne','one') == search
