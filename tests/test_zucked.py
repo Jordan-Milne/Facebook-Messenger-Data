6540dce726bb6a8eef02fa2202b675340c111b26
Jordan <jordan.milne2@gmail.com>
2020-05-11 12:14:31 -0400

    test fixed

--- tests/test_zucked.py
+++ tests/test_zucked.py
@@ -11,9 +11,11 @@ def test_top_convos():
     ms = zd.read_messages
     assert ms.top_convos('Jordan Milne') == top_convo
 
+# this test below passed locally but failed a lot on travis. I finally realized that the virtual machine travis-ci
+# uses to test code is in a different timezone setting and was reading the timestamp_ms as a different time, failing the test.
 def test_search_messages():
     search = [{'Message': 'hi hi hi lol lol one',
                'Sent to': 'Zark Muckerberg',
-               'Date': '2020-05-11 11:59:15'}]
+               'Date': '2020-05-11 15:59:15'}]
     ms = zd.read_messages
     assert ms.search_messages('Jordan Milne','one') == search
