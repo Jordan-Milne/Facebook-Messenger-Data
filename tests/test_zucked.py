199c84f5b671f065c81508835b41ddae33237b78
Jordan <jordan.milne2@gmail.com>
2020-05-11 12:06:00 -0400

    update pytest search_messages

--- tests/test_zucked.py
+++ tests/test_zucked.py
@@ -14,6 +14,6 @@ def test_top_convos():
 def test_search_messages():
     search = [{'Message': 'hi hi hi lol lol one',
                'Sent to': 'Zark Muckerberg',
-               'Date': '2016-06-09 11:46:54'}]
+               'Date': '2020-05-11 11:59:15'}]
     ms = zd.read_messages
     assert ms.search_messages('Jordan Milne','one') == search
